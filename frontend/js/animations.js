e7735e757d1f229e43aec52225e96a23c3b20e55
ljeandin <luciejeandin@gmail.com>
2020-11-27 16:45:20 +0100

    using text areas instead of input for posting and commenting

--- frontend/js/animations.js
+++ frontend/js/animations.js
@@ -205,3 +205,32 @@ function focusBtnGif() {
 function blurBtnGif() {
     document.getElementById("labelGif").style.backgroundColor = "#ecf0f3"; //color correspond to $background-color in sass/abstracts/variables
 }
+
+/******/
+function makeExpandingArea(container) {
+    var area = container.querySelector("textarea");
+    var span = container.querySelector("span");
+    if (area.addEventListener) {
+        area.addEventListener(
+            "input",
+            function () {
+                span.textContent = area.value;
+            },
+            false
+        );
+        span.textContent = area.value;
+    } else if (area.attachEvent) {
+        // IE8 compatibility
+        area.attachEvent("onpropertychange", function () {
+            span.innerText = area.value;
+        });
+        span.innerText = area.value;
+    }
+    // Enable extra CSS
+    container.className += "active";
+}
+var areas = document.querySelectorAll(".expandingArea");
+var l = areas.length;
+while (l--) {
+    makeExpandingArea(areas[l]);
+}
