fdc69980170132faab1a227db4c141b07962f35a
ljeandin <luciejeandin@gmail.com>
2020-12-02 16:43:43 +0100

    defining routes and views

---
+++ frontend/vue/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Feed from "../views/Feed.vue";
+import Login from "../views/Login.vue";
+import Signup from "../views/Signup.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "Login",
+        component: Login,
+    },
+    {
+        path: "/signup",
+        name: "Signup",
+        component: Signup,
+    },
+    {
+        path: "/feed",
+        name: "Feed",
+        component: Feed,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
