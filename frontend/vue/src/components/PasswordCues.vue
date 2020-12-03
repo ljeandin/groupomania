0714add764fcc33c5552f5e7e87372f34fafa42e
ljeandin <luciejeandin@gmail.com>
2020-12-03 07:29:43 +0100

    password cues beginning of implmentation

---
+++ frontend/vue/src/components/PasswordCues.vue
@@ -0,0 +1,169 @@
+<template>
+    <ul id="message" class="message">
+        <p id="validationTitle">Le mot de passe doit contenir :</p>
+        <li id="letter" class="invalid">Une minuscule</li>
+        <li id="capital" class="invalid">Une majuscule</li>
+        <li id="number" class="invalid">Un chiffre</li>
+        <li id="length" class="invalid">6 charactères minimum</li>
+    </ul>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+    name: 'PasswordCues',
+    setup(){
+        const state = reactive({
+
+        })
+
+        function passwordCueBlur(){
+            let letter = document.getElementById("letter");
+            let capital = document.getElementById("capital");
+            let number = document.getElementById("number");
+            let length = document.getElementById("length");
+
+            document.getElementById("message").style.display = "none";
+        }
+
+        function passwordCueFocus(){
+            let letter = document.getElementById("letter");
+            let capital = document.getElementById("capital");
+            let number = document.getElementById("number");
+            let length = document.getElementById("length");
+            
+            document.getElementById("message").style.display = "block";
+        }
+
+        function passwordCueKeyup(){
+            let letter = document.getElementById("letter");
+            let capital = document.getElementById("capital");
+            let number = document.getElementById("number");
+            let length = document.getElementById("length");   
+            
+            // Validate lowercase letters
+            var lowerCaseLetters = /[a-z]/g;
+            if (myInput.value.match(lowerCaseLetters)) {
+                letter.classList.remove("invalid");
+                letter.classList.add("valid");
+            } else {
+                letter.classList.remove("valid");
+                letter.classList.add("invalid");
+            }
+
+            // Validate capital letters
+            var upperCaseLetters = /[A-Z]/g;
+            if (myInput.value.match(upperCaseLetters)) {
+                capital.classList.remove("invalid");
+                capital.classList.add("valid");
+            } else {
+                capital.classList.remove("valid");
+                capital.classList.add("invalid");
+            }
+
+            // Validate numbers
+            var numbers = /[0-9]/g;
+            if (myInput.value.match(numbers)) {
+                number.classList.remove("invalid");
+                number.classList.add("valid");
+            } else {
+                number.classList.remove("valid");
+                number.classList.add("invalid");
+            }
+
+            // Validate length
+            if (myInput.value.length >= 6) {
+                length.classList.remove("invalid");
+                length.classList.add("valid");
+            } else {
+                length.classList.remove("valid");
+                length.classList.add("invalid");
+            }
+
+            //changing password message
+            if (
+                letter.classList.contains("valid") &&
+                capital.classList.contains("valid") &&
+                number.classList.contains("valid") &&
+                length.classList.contains("valid")
+            ) {
+                document.getElementById("validationTitle").value = "Le mot de passe est valide";
+            }
+        }
+
+        return{
+            state,
+        }
+    }
+}
+</script>
+
+/***Password validation***/
+let myInput = document.getElementById("mot_de_passe");
+let letter = document.getElementById("letter");
+let capital = document.getElementById("capital");
+let number = document.getElementById("number");
+let length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function () {
+    document.getElementById("message").style.display = "block";
+};
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function () {
+    document.getElementById("message").style.display = "none";
+};
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function () {
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if (myInput.value.match(lowerCaseLetters)) {
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+    } else {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+    }
+
+    // Validate capital letters
+    var upperCaseLetters = /[A-Z]/g;
+    if (myInput.value.match(upperCaseLetters)) {
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+    } else {
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+    }
+
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if (myInput.value.match(numbers)) {
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+    } else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+    }
+
+    // Validate length
+    if (myInput.value.length >= 6) {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+    } else {
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+    }
+
+    //changing password message
+    if (
+        letter.classList.contains("valid") &&
+        capital.classList.contains("valid") &&
+        number.classList.contains("valid") &&
+        length.classList.contains("valid")
+    ) {
+        document.getElementById("validationTitle").innerHTML = "Le mot de passe est valide";
+    }
+};
--- frontend/vue/src/views/Signup.vue
+++ frontend/vue/src/views/Signup.vue
@@ -1,18 +1,105 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<body class="inscription">
+    <Header />
+    <main>
+        <div class="bloc">
+            <form>
+                <div class="container">
+                    <section class="name">
+                        <div>
+                            <label for="nom">Nom</label>
+                            <input type="text" placeholder="ex : Durand" name="nom" id="nom" required />
+                        </div>
+                        
+                        <div>
+                            <label for="prenom">Prénom</label>
+                            <input type="text" placeholder="ex : Marie" name="prenom" id="prenom" required />
+                        </div> 
+                    </section>
+                        
+                    <div>
+                        <label for="email">E-mail</label>
+                        <input type="text" placeholder="ex : martinmarie@email.com" name="email" id="email" required />
+                    </div>
+                    
+                    <div>
+                        <label for="mot_de_passe">Mot de passe</label>
+                        <section class="passwordLine">
+                            <input 
+                                id="mot_de_passe" 
+                                type="password" 
+                                placeholder="Mot de passe" 
+                                name="nom"
+                                @blur="passwordCueBlur"
+                                @focus="passwordCueFocus"
+                                @keyup="passwordCueKeyup"
+                                required 
+                            />
+                            <i id="icon" class="material-icons" @click="seePassword()" @keypress="seePassword()" tabindex="0">visibility_off</i>
+                        </section>
+                    </div>
+                    <PasswordCues />
+                    
+                    <div class="imgUpload">
+                        <p class="label">Photo de profil <b>(vous pourrez en changer)</b></p>    
+                        <div class="imgUpload__line">
+                            <label for="télécharger_avatar" class="custom-file-upload" id="labelUploadAvatar" tabindex="-1" >
+                                Parcourir les fichiers
+                                <input type="file" id="télécharger_avatar" name="avatar" accept="image/*" onfocus="focusBtnUploadAvatar()" onblur="blurBtnUploadAvatar()" tabindex="0"/>
+                            </label>
+                            <!--This is the default avatar, replaced by user's avatar-->
+                            <img class="avatar" src="../assets/images/avatar_default.png" alt=""/>
+                        </div>
+                    </div>
+                    <button class="formSubmit" type="submit">Inscription</button>
+                </div>
+
+                <div class="container">
+                    <span class="formQuestion">Déjà inscrit·e ?</span>
+                    <button class="formExit" type="button">Connexion</button>
+                </div>
+            </form>
+        </div>
+    </main>
+</body>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { reactive } from 'vue';
+import Header from '../components/Header';
+import PasswordCues from '../components/Password';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  
+    setup(){
+        const state = reactive ({
+            name: 'Signup',
+            components : {
+                Header,
+                PasswordCues,
+            },
+        })
+
+        
+
+        function seePassword() {
+            let icon = document.getElementById("icon");
+            let password = document.getElementById("mot_de_passe");
+            //if password is hidden (with type=password in HTML)
+            if (password.type === "password") {
+                password.type = "text"; //show password with type=text
+                icon.innerHTML = "visibility"; //change icon
+            } else {
+                //if password is visible
+                password.type = "password"; //hide password
+                icon.innerHTML = "visibility_off"; // change icon
+            }
+        }
+
+        return {
+            state,
+            seePassword
+        }
+    }
 }
 </script>
