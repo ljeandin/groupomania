da89093c58a1a19ee9ab537c0c95a5eade604b3b
ljeandin <luciejeandin@gmail.com>
2021-01-12 09:51:25 +0100

    frontend modifications on login page

--- frontend/vue/src/views/Login.vue
+++ frontend/vue/src/views/Login.vue
@@ -62,10 +62,49 @@
 
 <script>
 import { reactive} from 'vue';
+import { onMounted } from 'vue';
+
 
 export default {
     name: "login",
     setup(){
+        /**Logo animations**/
+        onMounted(() => {
+            document.onreadystatechange = () => {
+                //animationg the logo with the globe icon
+                function animateSgv(id, delay, delayIncrement, delayFill) {
+                    const logo = document.getElementById(id);
+                    const logoPaths = document.querySelectorAll(`#${id} path`);
+                    for (let i = 0; i < logoPaths.length; i++) {
+                        logoPaths[i].style.strokeDasharray = logoPaths[i].getTotalLength();
+                        logoPaths[i].style.strokeDashoffset = logoPaths[i].getTotalLength();
+                        logoPaths[i].style.animation = `line-anim 1s ease-out forwards ${delay}s`;
+                        delay += delayIncrement;
+                        console.log(delay);
+                    }
+                    logo.style.animation = `fill 1s linear forwards ${delayFill}s`;
+                }
+                // Set the id of SVG, delay time in seconds to start animation, delay between each stroke animation and delay between stroke and fill animations
+                animateSgv("logoIcon", 0, 0.1, 1.1);
+            }
+        })
+
+        onMounted(() => {
+            function animateSgv(id, delay, delayIncrement) {
+                const logo = document.getElementById(id);
+                const logoPaths = document.querySelectorAll(`#${id} path`);
+                for (let i = 0; i < logoPaths.length; i++) {
+                    logoPaths[i].style.strokeDasharray = logoPaths[i].getTotalLength();
+                    logoPaths[i].style.strokeDashoffset = logoPaths[i].getTotalLength();
+                    logoPaths[i].style.animation = `line-anim 2s ease forwards ${delay}s`;
+                    delay += delayIncrement;
+                    console.log(delay);
+                }
+                logo.style.animation = `fill 0.5s ease-out forwards ${delay}s`;
+            }
+            // Set the id of SVG, delay time in seconds to start animation and delay between each animation
+            animateSgv("logoTypography", 0, 0.1);
+        })
         const state = reactive ({
         })
 
