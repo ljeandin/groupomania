a8b58d48fdde8f1e6af304391961a104b473696e
ljeandin <luciejeandin@gmail.com>
2021-01-21 09:03:15 +0100

    comments

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,12 +4,13 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 exports.create_an_account = function (req, res) {
-    //Check if there is content
+    //if there's no content sent, error
     if (!req.body) {
         res.status(400).send({
             message: "You must fill-in the form!",
         });
     } else {
+        //hash password
         bcrypt
             .hash(req.body.password, 10) //password hashing
             .then((hash) => {
@@ -39,13 +40,19 @@ exports.connect_to_account = function (req, res) {
     const email = req.body.email;
     const password = req.body.password;
 
+    //if email and password are filled-in
     if (email && password) {
+        //get all infos about the user that has the email
         sql.query("SELECT * FROM users WHERE email = ?", email, (error, results, _fields) => {
+            //if the email does exist in the db
             if (results.length > 0) {
+                //compare the given password to one in the db
                 bcrypt.compare(password, results[0].password).then((valid) => {
+                    // if it doesn't match, throw error
                     if (!valid) {
                         res.status(401).json({ message: "Wrong password" });
                     } else {
+                        //if it matches, send the infos and create auth token
                         console.log(email, "is logged-in");
                         res.status(200).json({
                             userId: results[0].id,
@@ -57,44 +64,12 @@ exports.connect_to_account = function (req, res) {
                     }
                 });
             } else {
-                res.status(401).json({ message: "One of your field is wrong" });
+                // if the email doesn't match, throw error
+                res.status(401).json({ message: "Wrong email adress" });
             }
         });
     } else {
+        //if the form isn't filled-in, throw error
         res.status(500).json({ message: "You must fill-in the form" });
     }
-    /*User.findOne(req.body.email, (err) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(404).send({
-                    message: "No user found with this email : " + req.body.email,
-                });
-            } else {
-                res.status(500).send({
-                    message: "An error happened when trying to retrieve user with this email : " + req.body.email,
-                });
-            }
-        } else {
-            bcrypt
-                .compare(req.body.password, result[0].password) //else comparing password to see if it matches
-                .then((valid) => {
-                    if (!valid) {
-                        //if invalid, error message
-                        res.status(401).send({
-                            message: "Wrong password !",
-                        });
-                    }
-                    res.status(200).json({
-                        userId: result[0].id,
-                        token: jwt.sign(
-                            // creating new token with user ID, token key and expiration limit
-                            { userId: result[0].id },
-                            "RANDOM_TOKEN_SECRET",
-                            { expiresIn: "24h" }
-                        ),
-                    });
-                })
-                .catch((error) => res.status(500).json({ error }));
-        }
-    });*/
 };
